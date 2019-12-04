48093f6a824193879fed683326d190df05dffbfc
raiden808 <raiden.nalasa@gmail.com>
2019-12-05 06:03:09 +0800

    cleaned app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from 'react-bulma-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <div className="field">
+          <label className="label">Search a Podcast!</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Insert RSS" />
+          </div>
+        </div>
+      </div>
   );
 }
 
